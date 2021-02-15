e904cb44347542211dabbbe20126efbab4a2d38c
Zachary Munson <zmunson85@gmail.com>
2021-02-15 14:55:51 -0700

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace vmf.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
