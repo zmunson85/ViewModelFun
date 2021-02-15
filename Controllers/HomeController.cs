8b77395827574d6778136a01bfe28ae7b4fa9f3b
Zachary Munson <zmunson85@gmail.com>
2021-02-15 16:00:22 -0700

    completed added some css

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,16 +11,76 @@ namespace vmf.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        [HttpGet]
+        [Route("")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        [Route("users")]
+        public IActionResult Users()
+        {
+            Individual personOne = new Individual()
+            {
+                FirstName = "Moose",
+                LastName = "Phillips"
+            };
+            Individual personTwo = new Individual()
+            {
+                FirstName = "Sarah",
+                LastName = ""
+            };
+            Individual personThree = new Individual()
+            {
+                FirstName = "Jerry",
+                LastName = ""
+            };
+            Individual personFour = new Individual()
+            {
+                FirstName = "Renee",
+                LastName = "Ricky"
+            };
+            Individual personFive = new Individual()
+            {
+                FirstName = "Barbarah",
+                LastName = " "
+            };
+
+            List<Individual> viewModel = new List<Individual>()
+            {
+                personOne, personTwo, personThree, personFour, personFive
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpGet("numbers")]
+        public IActionResult Numbers()
+        {
+            int[] numList = new int[] { 1, 2, 3, 10, 43, 5 };
+            Number firstNumber = new Number()
+            {
+                NumberList = numList
+            };
+            return View(firstNumber);
+        }
+
+        [HttpGet("user")]
+
+        public IActionResult User()
+        {
+            Person nPerson = new Person()
+            {
+                FirstName = "Moose",
+                LastName = "Phillips"
+            };
+
+            ViewBag.User = nPerson;
+
+            return View(nPerson);
         }
 
         public IActionResult Privacy()
@@ -31,6 +91,7 @@ namespace vmf.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+                                                            //request current id, request context trace Id similar to validations
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace vmf.Models
+{
+    public class Individual
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Number
+    {
+        public int[] NumberList { get; set; }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
